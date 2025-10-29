14672f4a7d3913d2acd619caea584799e37107e2
PEDAR MORDEGAN <mahbodsediq8144@gmail.com>
2025-10-29 23:30:33 +0330

    Base64.py

---
+++ Base64.py
@@ -0,0 +1,18 @@
+import base64
+
+def base64_encode(text):
+    message_bytes = text.encode('utf-8')
+    base64_bytes = base64.b64encode(message_bytes)
+    return base64_bytes.decode('utf-8')
+
+def base64_decode(encoded_text):
+    base64_bytes = encoded_text.encode('utf-8')
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes.decode('utf-8')
+
+message = "MAHBOD"
+encoded = base64_encode(message)
+decoded = base64_decode(encoded)
+
+print("Encoded:", encoded)
+print("Decoded:", decoded)
